7f91aba07baa1e96b7d05edf2ee0cc1bb57cb8cc
nhubaole <okamisameko7123@gmail.com>
2022-10-18 11:31:52 +0700

    Tạo base MVVM

---
+++ MusicPlayer/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+           //code xử lý
+        }
+    }
+}
