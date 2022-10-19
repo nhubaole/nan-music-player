69fb83f928fbb47f55c66af32e31bc8646c50be5
Anh Hà Mai <21520003@ms.uit.edu.vn>
2022-10-19 21:38:23 +0700

    UCControlBar

--- MusicPlayer/UserControls/ControlBar.xaml.cs
+++ MusicPlayer/UserControls/ControlBar.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MusicPlayer.UserControl
+namespace MusicPlayer.UserControls
 {
     /// <summary>
     /// Interaction logic for ControlBar.xaml
