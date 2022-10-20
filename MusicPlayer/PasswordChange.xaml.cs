6eddbcc223a6c56e144854f3d91b91d5d174ef91
Anh Hà Mai <21520003@ms.uit.edu.vn>
2022-10-20 16:35:44 +0700

    PasswordChange

--- MusicPlayer/PasswordChange.xaml.cs
+++ MusicPlayer/PasswordChange.xaml.cs
@@ -2,28 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MusicPlayer
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PasswordChange.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PasswordChange : Window
     {
-        public MainWindow()
+        public PasswordChange()
         {
-            PasswordChange passwordChange = new PasswordChange();
-            passwordChange.ShowDialog();                
+            InitializeComponent();
         }
     }
 }
