3026ff02db8640ebd8e4d542dc418a9815fc0901
Nhi Huỳnh Ngọc Ý <21522423@ms.uit.edu.vn>
2022-10-28 13:48:42 +0700

    fix MainVM

---
+++ MusicPlayer/ViewModel/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+namespace MusicPlayer.ViewModel
+{
+    public class LoginViewModel:BaseViewModel
+    {
+        public ICommand handleLoginCommand_1 { get; set; }//dang nhap thi vao MainWindow
+        public ICommand handleLoginCommand_2 { get; set; }//dang ki thi vao form dang ki 
+        public LoginViewModel()
+        {
+            handleLoginCommand_1 = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                var w = p as Window;
+                if (w != null)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    w.Close();
+                }
+            });
+            handleLoginCommand_2 = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                var w = p as Window;
+                if (w != null)
+                {
+                    SignUp signUp = new SignUp();
+                    signUp.Show();
+                    w.Close();
+                }
+            });
+        }
+    }
+}
--- MusicPlayer/ViewModel/MainViewModel.cs
+++ MusicPlayer/ViewModel/MainViewModel.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MusicPlayer.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public bool IsLoaded  = false;
+        public ICommand LoadedWindowCommand { get; set; }
         public MainViewModel()
         {
-           Upload up = new Upload();    
-            up.ShowDialog();    
+            LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { 
+                IsLoaded = true;
+                p.Hide();
+                Login login = new Login();
+                login.ShowDialog();
+                p.Show();
+            });    
         }
     }
 }
