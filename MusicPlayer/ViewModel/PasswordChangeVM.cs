b8595ac1bc7bed02cd1796d0a7d4600afd2e190d
nhubaole <okamisameko7123@gmail.com>
2022-11-20 22:34:34 +0700

    handle button PasswordChange Window

---
+++ MusicPlayer/ViewModel/PasswordChangeVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MusicPlayer.ViewModel
+{
+    internal class PasswordChangeVM : BaseViewModel
+    {
+        public ICommand savePasswordCommand { get; set; }
+        public ICommand closePasswordChangeCommand { get; set; }
+        public PasswordChangeVM()
+        {
+            savePasswordCommand = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                var w = p as Window;
+                if (w != null)
+                {
+                    MessageBox.Show("Đổi mật khẩu thành công!");
+                    w.Close();
+                }
+            });
+
+            closePasswordChangeCommand = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                if (MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Xác nhận", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    var w = p as Window;
+                    if (w != null)
+                    {
+                        w.Close();
+                    }
+                }
+            });
+        }
+    }
+}
--- MusicPlayer/ViewModel/ProfileVM.cs
+++ MusicPlayer/ViewModel/ProfileVM.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MusicPlayer.ViewModel
 {
-    internal class ProfileVM
+    internal class ProfileVM : BaseViewModel
     {
+        public ICommand handleChangePasswordCommand { get; set; }
+        public ProfileVM()
+        {
+            handleChangePasswordCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                PasswordChange passwordChange = new PasswordChange();
+                passwordChange.ShowDialog();
+            });
+        }
     }
 }
