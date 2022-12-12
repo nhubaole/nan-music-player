7126b07dab692cad049c83dcd56beb203d21972f
nhubaole <okamisameko7123@gmail.com>
2022-12-12 22:20:34 +0700

    Handle duration slider

--- MusicPlayer/UserControls/UCPlayMusic.xaml.cs
+++ MusicPlayer/UserControls/UCPlayMusic.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace MusicPlayer.UserControls
 {
@@ -24,12 +26,38 @@ namespace MusicPlayer.UserControls
     /// </summary>
     public partial class UCPlayMusic : UserControl
     {
-        public static MediaElement audio;
+        public static MediaElement audio = Application.Current.TryFindResource("audio") as MediaElement;
+        public static BackgroundWorker bw;
+        public static Slider sliderPlay = Application.Current.TryFindResource("slPlay") as Slider;
+        public static TextBlock position = Application.Current.TryFindResource("position") as TextBlock;
         public UCPlayMusic()
         {
             InitializeComponent();
             this.DataContext = this;
-            
+            sliderPlay.PreviewMouseUp += SliderPlay_PreviewMouseUp;
+        }
+
+        private void SliderPlay_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            selectedSong.POSITION = sliderPlay.Value;
+            audio.Position = new TimeSpan(0, 0, (int)selectedSong.POSITION);
+            position.Text = new TimeSpan(0, (int)(selectedSong.POSITION / 60), (int)(selectedSong.POSITION % 60)).ToString(@"mm\:ss");
+        }
+
+        private static void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                bw.ReportProgress(1);
+            }
+        }
+
+        private static void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            selectedSong.POSITION = audio.Position.TotalSeconds;
+            sliderPlay.Value = (double)selectedSong.POSITION;
+            position.Text = new TimeSpan(0, (int)(selectedSong.POSITION / 60), (int)(selectedSong.POSITION % 60)).ToString(@"mm\:ss");
         }
 
         static SONG selectedSong;
@@ -40,6 +68,7 @@ namespace MusicPlayer.UserControls
             set
             {
                 selectedSong = value;
+                
                 var singerName = Application.Current.TryFindResource("singerName") as TextBlock;
                 singerName.Text = selectedSong.SINGERNAME;
 
@@ -54,29 +83,33 @@ namespace MusicPlayer.UserControls
                 imgURL.Source = bitmapImage;
 
                 DownloadFileToPlay(selectedSong);
+                selectedSong.POSITION = audio.Position.TotalSeconds;
+                sliderPlay.Value = (double)selectedSong.POSITION;
+                position.Text = new TimeSpan(0, (int)(selectedSong.POSITION / 60), (int)(selectedSong.POSITION % 60)).ToString(@"mm\:ss");
 
-                audio = Application.Current.TryFindResource("audio") as MediaElement;
                 audio.Source = new Uri(selectedSong.SAVEPATH);
-                
                 audio.MediaOpened += Audio_MediaOpened;
                 audio.Play();
-
-                selectedSong.POSITION = 0;
-                var position = Application.Current.TryFindResource("position") as TextBlock;
-                position.Text = new TimeSpan(0, (int)(selectedSong.POSITION / 60), (int)(selectedSong.POSITION % 60)).ToString(@"mm\:ss"); 
             }
         }
 
         private static void Audio_MediaOpened(object sender, RoutedEventArgs e)
         {
+            bw = new BackgroundWorker();
+            bw.WorkerSupportsCancellation = true;
+            bw.WorkerReportsProgress = true;
+            bw.ProgressChanged += Bw_ProgressChanged;
+            bw.DoWork += Bw_DoWork;
             selectedSong.DURATION = audio.NaturalDuration.TimeSpan.TotalSeconds;
             var duration = Application.Current.TryFindResource("duration") as TextBlock;
             duration.Text = new TimeSpan(0, (int)(selectedSong.DURATION / 60), (int)(selectedSong.DURATION % 60)).ToString(@"mm\:ss");
+            sliderPlay.Maximum = (double)selectedSong.DURATION;
+
+            bw.RunWorkerAsync();
         }
 
         public static void DownloadFileToPlay(SONG selectedSong)
         {
-            
             string path = AppDomain.CurrentDomain.BaseDirectory + "Song\\" + selectedSong.SONGNAME + ".mp3";
             selectedSong.SAVEPATH = path;
             if (!File.Exists(path))
